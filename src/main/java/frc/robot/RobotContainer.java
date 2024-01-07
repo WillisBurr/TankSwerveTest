34df26f5e88ee731dd02da1cf37baf2cc5108d17
WillisBurr <williamcebrito@gmail.com>
2024-01-06 19:51:30 -0600

    finally saving forwards/backwards properly

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,8 +4,10 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.OperatorConstants;
@@ -26,7 +28,7 @@ public class RobotContainer {
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
       new CommandXboxController(OperatorConstants.PRIMARY_CONTROLLER_PORT);
-
+  private static final SendableChooser<SequentialCommandGroup> M_AUTOMODE_CHOOSER = new SendableChooser<>();
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     DRIVE_SUBSYSTEM.setDefaultCommand(
@@ -56,7 +58,14 @@ public class RobotContainer {
     // cancelling on release.
     m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
   }
-
+  private void autoModeChooser() {
+    M_AUTOMODE_CHOOSER.addOption("Do nothing", new SequentialCommandGroup());
+    M_AUTOMODE_CHOOSER.addOption("Drive forward", new SequentialCommandGroup(DRIVE_SUBSYSTEM.run((() -> DRIVE_SUBSYSTEM.set(0.5,0.0))
+    .withTimeout(5)
+    .andThen(()->DRIVE_SUBSYSTEM.stop()))
+    ));
+    
+  }
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
    *
@@ -64,6 +73,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return null;
+    return M_AUTOMODE_CHOOSER.getSelected();
   }
 }
